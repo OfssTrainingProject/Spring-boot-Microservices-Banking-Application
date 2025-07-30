b496d255a1db47b5cab29565572895cb507b3584
Avni Aggarwal <109527910+aggavni4@users.noreply.github.com>
2025-07-30 12:00:08 +0530

    Revert "Wip"

--- account-service/src/main/java/com/bank/exception/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bank.exception;
-
-public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException(String accNo) {
-        super("Account not found: " + accNo);
-    }
-}
